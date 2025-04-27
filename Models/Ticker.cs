992c33237d5892cf010f7b800c601aa3c8f7efb1
Hodladi <hodladi@hodla.nu>
2025-04-27 19:50:08 +0200

    Added Current BTC value in USD and EUR

---
+++ Models/Ticker.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpLN.Models;
+
+public class Ticker
+{
+	public string Amount { get; set; }
+	public string SourceCurrency { get; set; }
+	public string TargetCurrency { get; set; }
+	public string High { get; set; }
+	public string Low { get; set; }
+}
